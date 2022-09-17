4721b81449df6cebe67e1070d42013af7eb2579a
Ihor-Slastonov <jammik000@gmail.com>
2022-09-17 03:23:46 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
 
 inputRef.addEventListener('input', onChangeSize);
-spanRef.style.fontSize = `${inputRef.value}px`;
+spanRef.style.fontSize = inputRef.value + 'px'
 
 function onChangeSize(event) {
     spanRef.style.fontSize = `${inputRef.value}px`;
